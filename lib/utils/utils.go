36db8eab918e25beed8fb3cb5c2f841e369095da
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2024-06-17 14:39:02 +0200

    utils : create contains

--- lib/utils/utils.go
+++ lib/utils/utils.go
@@ -12,3 +12,12 @@ func RemoveDuplicates(elements []string) []string {
 	}
 	return result
 }
+
+func Contains(slice []string, item string) bool {
+	for _, a := range slice {
+		if a == item {
+			return true
+		}
+	}
+	return false
+}
