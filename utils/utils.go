4f6cd3e345d10d95decb6b13fb885c87ed5fe483
QU35T-code <github@qu35t-mail.simplelogin.com>
2024-06-18 13:49:29 +0200

    feat(company): Add confirmation message

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"github.com/AlecAivazis/survey/v2"
+)
+
 func RemoveDuplicates(elements []string) []string {
 	encountered := map[string]bool{}
 	var result []string
@@ -11,4 +15,16 @@ func RemoveDuplicates(elements []string) []string {
 		}
 	}
 	return result
+}
+
+// AskConfirmation will request confirmation from the user
+func AskConfirmation(message string) bool {
+	var confirmation bool
+	prompt := &survey.Confirm{
+		Message: message,
+	}
+	if err := survey.AskOne(prompt, &confirmation); err != nil {
+		return false
+	}
+	return confirmation
 }
